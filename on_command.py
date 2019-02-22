18ef82d7df26bfe2ea13f4e6c649a1d6d332434b
juacrumar <juacrumar@gmail.com>
2019-02-22 12:10:41 +0100

    bugfix

--- on_command.py
+++ on_command.py
@@ -6,10 +6,12 @@ def select_command(tg_command, message_obj):
     if tg_command == "ip":
         return c.ip_lookup(message_obj.chatId)
     elif tg_command == "arxiv-query":
+        from components import arxiv_functions
         arxiv_id = message_obj.text.strip()
-        return c.arxiv_query_info(arxiv_id)
+        return arxiv_functions.arxiv_query_info(arxiv_id)
     elif tg_command == "arxiv-get":
+        from components import arxiv_functions
         arxiv_id = message_obj.text.strip()
-        return {'isfile' : True, 'filename' : c.arxiv_get_pdf(arxiv_id), 'delete' : True}
+        return {'isfile' : True, 'filename' : arxiv_functions.arxiv_get_pdf(arxiv_id), 'delete' : True}
 
 
