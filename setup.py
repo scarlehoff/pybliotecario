91405be2a2fd6de1fca59a02ee5ce8ea7c9bca6c
Juacrumar <juacrumar@gmail.com>
2019-07-29 19:21:43 +0200

    use hostname to name the bot

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup, find_packages
+import socket
+
+
+# By default we will use 'hostname' as the name for the script
+# with the first letter uppercased
+hostname = socket.gethostname()
+pybliotecario_name = hostname.capitalize()
 
 setup(
-        name="pybliotecario",
+        name=pybliotecario_name,
         version="1.0",
         package_dir = {'':'src'},
         packages=find_packages('src'),
 
         entry_points = {'console_scripts':
-            ['pybliotecario = pybliotecario.pybliotecario:main',]
+            [f'{pybliotecario_name} = pybliotecario.pybliotecario:main',]
             },
 )
+
+print(f'Installed pybliotecario as {pybliotecario_name}')
