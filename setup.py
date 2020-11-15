5101faf85a2129ac01ae37cbf5e3ce8d91dad637
Juacrumar <jumax9.gently@gmail.com>
2020-11-15 21:37:15 +0100

    bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if "--with_name" in sys.argv:
 README = (pathlib.Path(__file__).parent / "readme.md").read_text()
 setup(
     name=pybliotecario_name,
-    version="1.3",
+    version="2.0",
     author="Scarlehoff",
     author_email="juacrumar@lairen.eu",
     url="https://github.com/scarlehoff/pybliotecario",
